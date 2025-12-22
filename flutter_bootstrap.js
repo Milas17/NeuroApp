3db441d609ad5f89b8694331a1e33fa5e145ffb6
Milas17 <Milas17@users.noreply.github.com>
2025-12-22 01:53:50 +0000

    deploy: 292aa3b2ab5a4a72a1816fff5fd0f3952eb5ad14

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "560863674"
+    serviceWorkerVersion: "2213867045"
   }
 });
